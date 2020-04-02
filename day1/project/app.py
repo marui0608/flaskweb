7a88fe632b1c950295d8d21fe41a131b3256cd0d
Mary <1696815801@qq.com>
2020-04-02 17:53:26 +0800

    提交了数据库和css样式

--- day1/project/app.py
+++ day1/project/app.py
@@ -1,5 +1,32 @@
+import os,sys
+
 from flask import Flask,url_for,render_template
+from flask_sqlalchemy import SQLAlchemy
+import click
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = "sqlite:///"   # windows 平台
+else:
+    prefix = "sqlite:////"   # Mac,Linux 平台
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path,'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+#models 数据层
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(20))
+    year = db.Column(db.String(4))
+
+
+# 视图函数
 
 # @app.route('/')
 # @app.route('/home/')
@@ -21,7 +48,14 @@ def index():
     return render_template("index.html",name=name,movies=movies)
 
 
-
+# 自定义命令
+@app.cli.command()   # 命令注册
+@click.option('--drop',is_flag=True,help="先删除在创建")
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo("初始化数据库完成")
 
 
 # if __name__ == '__main__':
