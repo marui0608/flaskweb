fa81120d98291b654147af24c50077e100d80842
Mary <1696815801@qq.com>
2020-04-14 13:51:14 +0800

    Vuex&&主键精讲

--- vuePlugin/script.js
+++ vuePlugin/script.js
@@ -1,11 +1,21 @@
 import Vue from 'vue/dist/vue.js'
 import VueMathPlugin from './VueMathPlugin.js'
+import Vuex from 'Vuex'
+Vue.use(VueMathPlugin)   // 调用
+Vue.use(Vuex)    // 调用
 
-Vue.use(VueMathPlugin)
+var store = new Vuex.Store({
+    state:{message:'Hello!'},
+    mutations:{
 
+    },
+    actions:{},
+    getters:{},
+})
 new Vue({
     el:'#app',
     data:{
         item:50
-    }
+    },
+    store:store
 })
