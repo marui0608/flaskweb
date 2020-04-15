b577e323cbd6b7e247266941def5ae729dc81172
Mary <1696815801@qq.com>
2020-04-15 10:19:14 +0800

    vue实战&首页

---
+++ layout/src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Layout from './Layout.vue'
+import IndexPage from './pages/index.vue'
+
+// 调用
+Vue.use(VueRouter)
+// 实例化 VueRouter
+let router = new VueRouter({
+  mode:'history',   // 记录路由   可以让页面 回退
+  routes:[
+    {
+      path:'/',
+      component:IndexPage
+    }
+  ]
+})
+
+new Vue({
+  el: '#app',
+  router,
+  components:{
+    Layout
+  },
+  template:'<Layout/>'    // 对应Layout.vue里面所有的标签
+})
---
+++ layout/src/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        index 首页
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    
+}
+</script>
+
+
+
+<style>
+    
+</style>
